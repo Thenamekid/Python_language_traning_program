b8f0cc8aa724fee3f95a81c8bdd1b7e2a7958f4d
Thenamekid <77798301+Thenamekid@users.noreply.github.com>
2023-10-12 07:37:13 +0000

    Add files via upload

--- Python_language_traning_program.py
+++ Python_language_traning_program.py
@@ -4,7 +4,7 @@ language = "French" # what will be displayed, cange if you want
 words = ["hello", "thank you", "yes", "no", "book"] # this is case sensative
 facits = ["bonjour", "merci", "oui", "non", "livre"] # change this and the words to what words you want to be quized on
 old_words = words.copy()
-old_facits = facits.copy()
+old_facits = facits.copy() 
 
 def reset(): # to reset the wrong o meter
     global wrong_o_meter
@@ -25,22 +25,36 @@ while True:
     if answer != facit:
         print("WRONG!!! The answer was:", facit) # you can remove it saying the facit if you want
         wrong_o_meter = wrong_o_meter + 1
+        time.sleep(0.5)
+        input("Do you want to continue (just press enter if so)? ")
+        loop = 0
+        while True: # move the user so they can't see answer
+            loop = loop + 1
+            print("\n\n\n\n\n\n\n\n\n\n")
+            if loop == 4:
+                break
     else:
         print("Wow, thats correct. You said:", answer)
 
-        if len(words) <= 1 and len(facits) <= 1:
+        if len(words) <= 1:
             time.sleep(1)
             print("Nice, you have gone thruogh all your words!")
             time.sleep(1)
             print("Good job :)")
             if wrong_o_meter > 0:
                 time.sleep(1)
-                print("Thougt you did do ", wrong_o_meter, " errors...")
+                print("Though you did do", wrong_o_meter, "errors...")
             else:
                 print("You didn't do any wrong spellings!")
             time.sleep(2)
             print("Restarting...")
             time.sleep(2)
+            loop = 0
+            while True: # move the user so they can't see previous answer
+                loop = loop + 1
+                print("\n\n\n\n\n\n\n\n\n\n")
+                if loop == 5:
+                    break
             reset()
 
             words = old_words.copy() # copy the words to what you set them to in the beigining
